3c941b33f1d66ba8f2cc197b0fa3a36f554b2193
Caitlin Colgrove <ccolgrove841@gmail.com>
2011-12-04 19:09:39 -0800

    making naive bayes accept test docs and revisiona

--- CS229Java/src/bagofwords/MultinomialNaiveBayes.java
+++ CS229Java/src/bagofwords/MultinomialNaiveBayes.java
@@ -2,10 +2,14 @@ package bagofwords;
 
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import take_two.Revision;
+import take_two.TestDocument;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.process.CoreLabelTokenFactory;
 import edu.stanford.nlp.process.PTBTokenizer;
@@ -14,22 +18,26 @@ public class MultinomialNaiveBayes {
 	
 	public static final double ADD_K = 1;
 	public static final String UNK_TOKEN = "<UNKNOWN>";
-
-	public static Map<Integer, String> getDocumentMap() {
-		Map<Integer, String> testMap = new HashMap<Integer, String>();
-		testMap.put(1, "hello world");
-		testMap.put(2, "goodbye world");
-		testMap.put(3, "test test blah blah blah");
+	
+	public Map<String, Double> wordProbs;
+	
+	private static class Pair {
+		public double score;
+		public String paragraph;
 		
-		return testMap;
+		public Pair(String paragraph, double score) {
+			this.score = score;
+			this.paragraph = paragraph;
+		}
 	}
 	
-	public static Map<String, Integer> getEditWordCounts(List<String> edits) {
+	public Map<String, Integer> getWordCounts(List<Revision> revisions, List<TestDocument> documents) {
 		Map<String, Integer> counts = new HashMap<String, Integer>();
 		
 		CoreLabelTokenFactory factory = new CoreLabelTokenFactory();
 		
-		for(String edit : edits) {
+		for(Revision rev : revisions) {
+			String edit = rev.content;
 			PTBTokenizer<CoreLabel> ptbt = new PTBTokenizer<CoreLabel>(new StringReader(edit),
 		              factory, "");
 		    for (; ptbt.hasNext(); ) {
@@ -42,11 +50,28 @@ public class MultinomialNaiveBayes {
 		    }
 		}
 		
-		counts.put(UNK_TOKEN, 1);
+		for (TestDocument doc: documents) {
+			for (String paragraph : doc.paragraphs) {
+				PTBTokenizer<CoreLabel> ptbt = new PTBTokenizer<CoreLabel>(new StringReader(paragraph),
+		              factory, "");
+				for (; ptbt.hasNext(); ) {
+					String label = ptbt.next().originalText();
+			        if (!counts.containsKey(label)) {
+			        	counts.put(label, 0);
+			        }
+			    }	
+			}
+		}
+		
+		counts.put(UNK_TOKEN, 0);
+		
+		for (String key : counts.keySet()) {
+			counts.put(key, counts.get(key) + 1);
+		}
 		return counts;
 	}
 	
-	public static Map<String, Double> getEditWordProbs(Map<String, Integer> counts) {
+	public Map<String, Double> getWordProbs(Map<String, Integer> counts) {
 		double total = 0;
 		Map<String, Double> probs = new HashMap<String, Double>();
 		for (String key: counts.keySet()) {
@@ -60,7 +85,7 @@ public class MultinomialNaiveBayes {
 		return probs;
 	}
 	
-	public static double getSentenceProb(Map<String, Double> wordProbs, String sentence) {
+	public double getSentenceProb(Map<String, Double> wordProbs, String sentence) {
 		double logProb = 0;
 		CoreLabelTokenFactory factory = new CoreLabelTokenFactory();
 		PTBTokenizer<CoreLabel> ptbt = new PTBTokenizer<CoreLabel>(new StringReader(sentence),
@@ -76,28 +101,56 @@ public class MultinomialNaiveBayes {
 	    return logProb;
 	}
 	
-	public static String mostLikelySentence(Map<String, Double> wordProbs, Map<Integer, String> sentences) {
-		double maxProb = Double.NEGATIVE_INFINITY;
-		String maxSentence = "";
-		for (String sentence : sentences.values()) {
-			double prob = getSentenceProb(wordProbs, sentence);
-			if (prob > maxProb) {
-				maxProb = prob;
-				maxSentence = sentence;
-			}
+	public void calculateProbabilites(List<Revision> revisions, List<TestDocument> docs) {
+		Map<String, Integer> wordCounts = getWordCounts(revisions, docs);
+		wordProbs = getWordProbs(wordCounts);
+	}
+	
+	public List<String> mostLikelyParagraphs(TestDocument document) {
+	
+		List<Pair> paragraphs = new ArrayList<Pair>();
+		
+		for (String paragraph : document.paragraphs) {
+			double prob = getSentenceProb(wordProbs, paragraph);
+			paragraphs.add(new Pair(paragraph, prob));	
 		}
 		
-		return maxSentence;
+		Collections.sort(paragraphs, new Comparator<Pair>() {
+			@Override
+			public int compare(Pair o1, Pair o2) {
+				return -1*Double.compare(o1.score, o2.score);
+			}	
+		});
+		
+		List<String> orderedParagraphs = new ArrayList<String>();
+		for (Pair p: paragraphs) {
+			orderedParagraphs.add(p.paragraph);
+		}
+		
+		return orderedParagraphs;
 	}
 	
 	public static void main(String[] args) {
-		List<String> edits = new ArrayList<String>();
-		edits.add("hello");
-		edits.add("goodbye goodbye");
 		
-		Map<String, Integer> counts = getEditWordCounts(edits);
-		Map<String, Double> probs = getEditWordProbs(counts);
-		String mls = mostLikelySentence(probs, getDocumentMap());
+		TestDocument doc = new TestDocument();
+		doc.paragraphs.add("test test");
+		doc.paragraphs.add("hello goodbye");
+		doc.paragraphs.add("hi hi hi");
+		
+		Revision rev1 = new Revision("a", "b", "c",  null, "", "hello hello world test");
+		List<Revision> revs = new ArrayList<Revision>();
+		revs.add(rev1);
+		
+		List<TestDocument> documents = new ArrayList<TestDocument>();
+		documents.add(doc);
+		
+		MultinomialNaiveBayes mnb = new MultinomialNaiveBayes();
+		mnb.calculateProbabilites(revs, documents);
+		
+		List<String> mostLikely = mnb.mostLikelyParagraphs(doc);
+		
+		System.out.println(mostLikely);
+		
 	}
 	
 }
