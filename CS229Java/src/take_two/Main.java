de24ddf41bbb016330bd30cb1907888fec8e25d8
Julie Tibshirani <jtibs@stanford.edu>
2011-12-07 19:52:10 -0800

    pushing...

--- CS229Java/src/take_two/Main.java
+++ CS229Java/src/take_two/Main.java
@@ -12,15 +12,20 @@ import java.util.Map;
 public class Main {
   public static final String TEST_DIR = "../../../articles/";
   private static final String TRAIN_DIR = "../../../revhistories/revision_diffs_by_user/";
-  public static final String USER = "Hammersoft";
-
+  //public static final String USER = "Hammersoft";
+  public static final String USER = "SF007";
+  //public static final String USER = "The_Egyptian_Liberal";
+  //public static final String USER = "JerryOrr";
+  //public static final String USER = "Tangledorange";
+  
   public static void main(String[] args) {
     List<Revision> revisions = new ArrayList<Revision>();
     List<TestDocument> documents = new ArrayList<TestDocument>();
 
     // read in the train and test data
+    System.out.println("reading in train data!");
     XMLParser parser = new XMLParser();
-
+    int count = 0;
     File dir = new File(TRAIN_DIR + USER);
     for (File file : dir.listFiles()) {
       //System.err.println(file);
@@ -28,6 +33,7 @@ public class Main {
       if (revision != null) revisions.add(revision);
     }
 
+    System.out.println("reading in test data!");
     dir = new File(TEST_DIR);
     for (File file : dir.listFiles()) {
       String name = file.getName();
@@ -39,12 +45,14 @@ public class Main {
       document.id = name.substring(0, dash);
       documents.add(document);
     }
-
+    
     // prepare test data
+    System.out.println("calculating scores!");
     for (TestDocument document : documents)
       calculateScores(document, revisions);
     
     // train + test using leave-one-out cross-validation
+    System.out.println("testing!");
     MultinomialNaiveBayes classifier = new MultinomialNaiveBayes();
     for (TestDocument document : documents) {
       List<Revision> train = new ArrayList<Revision>(revisions);
@@ -53,13 +61,25 @@ public class Main {
           train.remove(revision);
       }
       
-      classifier.calculateProbabilities(revisions, documents);
-      List<String> prediction = classifier.mostLikelyParagraphs(document);
+      classifier.calculateProbabilities(train, documents);
+      List<MultinomialNaiveBayes.Pair> predictions = classifier.mostLikelyParagraphs(document);
       
       document.orderParagraphs();
-      System.out.println("~~~PREDICTION~~~\n" + prediction);
-      System.out.println("~~~ACTUAL~~~\n" + document.paragraphs.get(0));
-      break;
+      String actual = document.paragraphs.get(0);
+      
+      System.out.println("num paragraphs: " + predictions.size());
+      int rank = -1;
+      for (int i = 0; i < predictions.size(); i++) {
+        //System.out.println("~~~PREDICTION" + i + "~~~\n");
+        //System.out.println(predictions.get(i).paragraph);
+        System.out.println("score: " + predictions.get(i).score);
+        if (predictions.get(i).paragraph.equals(actual))
+          rank = i;
+      }
+      System.out.println("rank: " + rank);
+      System.out.println("PREDICTED:\n" + predictions.get(0).paragraph);
+      System.out.println("ACTUAL:\n" + actual);
+        
     }
   }
 
@@ -73,17 +93,22 @@ public class Main {
       if (! revision.pageId.equals(document.id))
         continue;
 
-      String[] split = revision.content.split("\\s+");
+      String[] revisionSplit = revision.content.split("\\s+");
+      String[] paragraphSplit = revision.content.split("\\s+");
       List<Double> jaccardScores = new ArrayList<Double>();
       for (int i = 0; i < document.paragraphs.size(); i++) {
         String paragraph = document.paragraphs.get(i);
 
         int matches = 0;
-        for (String word : split) {
+        for (String word : revisionSplit) {
           if (paragraph.contains(word) && word.length() > 0)
             matches++;
-        }     
-        jaccardScores.add((double) matches / split.length);
+        }
+        for (String word : paragraphSplit) {
+          if (revision.content.contains(word) && word.length() > 0)
+            matches++;
+        }
+        jaccardScores.add((double) matches / (revisionSplit.length + paragraphSplit.length));
       }
       double max = -1.0;
       int index = -1;
@@ -95,8 +120,8 @@ public class Main {
       }
 
       document.scores.set(index, document.scores.get(index) + 1);
-      System.out.println("\n~~~REVISION~~~\n" + revision.content);
-      System.out.println("\n~~~PARAGRAPH~~~\n" + document.paragraphs.get(index));
+      //System.out.println("\n~~~REVISION~~~\n" + revision.content);
+      //System.out.println("\n~~~PARAGRAPH~~~\n" + document.paragraphs.get(index));
     }
   }
 }
