4146d898951e85aa7535effba6c84cc69be1f119
Caitlin Colgrove <ccolgrove841@gmail.com>
2011-12-07 20:29:14 -0800

    changing gitignore

--- CS229Java/src/take_two/XMLParser.java
+++ CS229Java/src/take_two/XMLParser.java
@@ -1,20 +1,14 @@
 package take_two;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.StringBufferInputStream;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
